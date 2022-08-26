907a67cb1a9ddb98da7bbe7c21fce0bbbaefe197
mnishimori <mnishimori@yahoo.com.br>
2022-08-25 22:25:23 -0300

    Servlet com parâmetros

---
+++ src/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
@@ -0,0 +1,28 @@
+package br.com.alura.gerenciador.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class NovaEmpresaServlet
+ */
+@WebServlet("/novaEmpresa")
+public class NovaEmpresaServlet extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("Cadastrando nova empresa");
+        String nomeEmpresa = req.getParameter("nome");
+        PrintWriter out = resp.getWriter();
+        out.println("<html><body>Empresa " + nomeEmpresa + " cadastrada com sucesso!</body></html>");	
+	}
+
+}
