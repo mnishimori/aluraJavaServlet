0f2a98b54e8f6d9e73bd51e300fc4d275fd01cbe
mnishimori <mnishimori@yahoo.com.br>
2022-09-12 13:49:19 -0300

    feature: autenticação

--- src/br/com/alura/gerenciador/modelo/Banco.java
+++ src/br/com/alura/gerenciador/modelo/Banco.java
@@ -6,6 +6,7 @@ import java.util.List;
 public class Banco {
 	
 	private static List<Empresa> lista = new ArrayList<>();	
+	private static List<Usuario> listaUsuarios = new ArrayList<Usuario>();
 	private static Integer chaveSequencial = 1;
 
 	public void adiciona(Empresa empresa) {
@@ -18,14 +19,25 @@ public class Banco {
 	}
 	
 	static {
-	  Empresa empresa = new Empresa();
-	  empresa.setId(Banco.chaveSequencial++);
-	  empresa.setNome("Alura");
-	  Empresa empresa2 = new Empresa();
-	  empresa2.setId(Banco.chaveSequencial++);
-	  empresa2.setNome("Caelum");
-	  Banco.lista.add(empresa);
-	  Banco.lista.add(empresa2);
+		Empresa empresa = new Empresa();
+		empresa.setId(Banco.chaveSequencial++);
+		empresa.setNome("Alura");
+		Empresa empresa2 = new Empresa();
+		empresa2.setId(Banco.chaveSequencial++);
+		empresa2.setNome("Caelum");
+		Banco.lista.add(empresa);
+		Banco.lista.add(empresa2);
+
+		Usuario u1 = new Usuario();
+		u1.setLogin("nico");
+		u1.setSenha("12345");
+
+		Usuario u2 = new Usuario();
+		u2.setLogin("ana");
+		u2.setSenha("12345");
+
+		listaUsuarios.add(u1);
+		listaUsuarios.add(u2);
 	}
 
 	public void removerEmpresa(Integer id) {
@@ -48,6 +60,14 @@ public class Banco {
 		
 		return lista.stream()
 				.filter(e -> Integer.valueOf(e.getId()).equals(id))
+				.findFirst()
+				.orElse(null);
+	}
+
+	public Usuario existeUsuario(String login, String senha) {
+		
+		return listaUsuarios.stream()
+				.filter(u -> u.getLogin().equals(login) && u.getSenha().equals(senha))
 				.findFirst()
 				.orElse(null);
 	}
