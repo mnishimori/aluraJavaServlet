80b0addc397ac53d7654b82cf0db111c2393f5a7
mnishimori <mnishimori@yahoo.com.br>
2022-09-14 22:41:58 -0300

    feature: primeiro filtro

--- src/br/com/alura/gerenciador/acao/ListaEmpresas.java
+++ src/br/com/alura/gerenciador/acao/ListaEmpresas.java
@@ -6,7 +6,6 @@ import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import br.com.alura.gerenciador.modelo.Banco;
 import br.com.alura.gerenciador.modelo.Empresa;
@@ -15,14 +14,15 @@ public class ListaEmpresas implements Acao {
 	
 	public String executa(HttpServletRequest request, HttpServletResponse response) 
 			throws ServletException, IOException {
-		
-		
+
 		System.out.println("listando empresas");
 		
 		Banco banco = new Banco();
 		List<Empresa> empresas = banco.getEmpresas();
 		
 		request.setAttribute("empresas", empresas);
+		
+		long depois = System.currentTimeMillis();
 		
 		return "forward:listaEmpresas.jsp";
 	}
